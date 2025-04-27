0c18afcdc6de8747e0dcdb7c90cf549a9f0fb5ed
Toren Long <venlercode@gmail.com>
2025-04-27 12:59:44 -0500

    Dictation Testing

---
+++ frontend/my-project/app/dictation/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+import {useEffect, useState} from "react"
+type SpeechRecognitionType = any; // TODO: Replace with proper type
+
+export default function Page() {
+    const [speechReg, setSpeechReg] = useState<SpeechRecognitionType | null>(null);
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+            if (SpeechRecognition) {
+                const recognition = new SpeechRecognition();
+                recognition.continuous = true;
+                recognition.lang = "en-US";
+                recognition.interimResults = false;
+
+                recognition.onresult = (event: any) => {
+                    console.info(event.results);
+                    const result = event.results[event.resultIndex];
+                    if (result.isFinal) {
+                        const transcript = result[0].transcript
+                        console.log(`Speech recognized: ${transcript}`);
+                    }
+                };
+                setSpeechReg(recognition);
+            }
+        }
+    }, []);
+  function handleMicToggle(event: React.ChangeEvent<HTMLInputElement>) {
+    if (!speechReg) return;
+    
+    if (event.target.checked) {
+      speechReg.start();
+    } else {
+      speechReg.stop();
+    }
+  }
+
+
+  return (
+    <div>
+        Hello World!
+        <input type="checkbox" id="mic-toggle" className="sr-only" onChange={event => handleMicToggle(event)} />
+    </div>
+  );
+}
+declare global { // Remove Typescript errors
+    interface Window {
+      webkitSpeechRecognition: any;
+      SpeechRecognition: any;
+    }
+}
